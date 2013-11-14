efc31ce76e5df5da5f54e73379a443169bcc460c
Derek Montgomery <montgomerygraphics@gmail.com>
2013-11-14 14:21:04 -0600

    XD Mail Compiler Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XdMailCompiler::Application.config.secret_key_base = '8138f699287813f0e4abe018be31b1e44801d9cb90d2db0d7dc12704974589d7107132b9522a421ef55a97aa07c8180996bec928038e50542a2cd41c4f877ba6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XdMailCompiler::Application.config.session_store :cookie_store, key: '_xd-mail-compiler_session'
