668ba5b4f98ad3e55eb1a9deb93613ef0eb429de
Derek Montgomery <montgomerygraphics@gmail.com>
2014-01-03 10:48:38 -0600

    Completed exports

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -4,22 +4,23 @@ class ExportsController < ApplicationController
     require 'zip'
 
     @campaign   = Campaign.find(session[:campaign_id])
-    asset_types = {
-      stylesheets: @campaign.assets.stylesheets,
-      markup_docs: @campaign.assets.markups,
-           images: @campaign.assets.images
-    }
+    stylesheets = @campaign.assets.stylesheets
+    markup_docs = @campaign.assets.markups
+    images      = @campaign.assets.images
 
-    master_zip_file_name = "#{@campaign.name.parameterize rescue 'name'}_#{DateTime.now.strftime("%d%m%Y%H%M%S")}"
+    master_zip_file_name = "#{@campaign.name.parameterize rescue 'campaign'}_#{DateTime.now.strftime("%d%m%Y%H%M%S")}"
 
     tmp_zip = Tempfile.new master_zip_file_name
 
     Zip::OutputStream.open(tmp_zip.path) do |z|
-      asset_types.each do |key, value|
-        value.each do |asset|
-          z.put_next_entry asset.file_file_name
-          z.print IO.read("public/#{strip_query(asset.file.url)}")
-        end
+      markup_docs.each do |murecord|
+        z.put_next_entry murecord.file_file_name
+        mu_file = File.new("public#{strip_query murecord.file.url}")
+        ss_file = File.new("public#{strip_query stylesheets.first.file.url}")
+        content = Compiler::Markup.render mu_file, ss_file
+
+        # z.print IO.read("public/#{strip_query(asset.file.url)}")
+        z.print content.to_html
       end
     end
 
